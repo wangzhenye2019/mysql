c1d528593026b78f7bf1fb7bd206ba622cbd6a2c
wangzhenye2019 <wangzhenye2019@gmail.com>
2021-12-12 20:42:29 +0800

    修改建库删库脚本

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -3,10 +3,9 @@ user=root
 passwd=rootroot
 host=127.0.0.1
 
-
 for i in `seq 1 20`
 do
-sql1=`mysql -uroot -p $passwd -h $host -e "set default_storage_engine=InnoDB;create database if not existsitpux$i DEFAULT CHARACTER SET UTF8mb4 DEFAULT COLLATE utf8mb4_general_ci;"`
+sql1=`mysql -u$user -p$passwd -h$host -e "set default_storage_engine=InnoDB;create database if not exists itpux$i DEFAULT CHARACTER SET UTF8mb4 DEFAULT COLLATE utf8mb4_general_ci;"`
 echo "[info]第 $i 个数据库 itpux$i 创建成功"
 done
 echo "-----------------------------------"
--- scripts/drop_db.sh
+++ scripts/drop_db.sh
@@ -6,7 +6,7 @@ host=127.0.0.1
 
 for i in `seq 1 20`
 do
-sql1=`mysql -u $user -p $passwd -h $host -e "drop database if exists itpux$i"`
+sql1=`mysql -u$user -p$passwd -h$host -e "drop database if exists itpux$i"`
 echo "[info]第 $i 个数据库itpux$i 删除成功"
 done
 echo "-----------------------------------"
